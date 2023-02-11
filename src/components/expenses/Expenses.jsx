aa1a0f78f1744e881531f20b34161eb2c3c39796
rezaGhoreyshiWeb <rezaghoreyshi.contact@gmail.com>
2023-02-11 17:33:35 +0330

    ExpensesFilter

--- src/components/expenses/Expenses.jsx
+++ src/components/expenses/Expenses.jsx
@@ -1,31 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../styles/Expenses.css";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
 
 export default function Expenses({ items }) {
+  const [filteredYear, setFilteredYear] = useState(
+    new Date().getFullYear().toString()
+  );
+
+  const allYears = [
+    ...new Set(
+      items.map((item) => {
+        return new Date(item.date).getFullYear().toString();
+      })
+    ),
+  ];
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+    console.log(selectedYear);
+  };
+
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={items[0].title}
-        date={items[0].date}
-        amount={items[0].amount}
-      />
-      <ExpenseItem
-        title={items[1].title}
-        date={items[1].date}
-        amount={items[1].amount}
-      />
-      <ExpenseItem
-        title={items[2].title}
-        date={items[2].date}
-        amount={items[2].amount}
-      />
-      <ExpenseItem
-        title={items[3].title}
-        date={items[3].date}
-        amount={items[3].amount}
-      />
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          allYears={allYears}
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpenseItem
+          title={items[0].title}
+          date={items[0].date}
+          amount={items[0].amount}
+        />
+        <ExpenseItem
+          title={items[1].title}
+          date={items[1].date}
+          amount={items[1].amount}
+        />
+        <ExpenseItem
+          title={items[2].title}
+          date={items[2].date}
+          amount={items[2].amount}
+        />
+        <ExpenseItem
+          title={items[3].title}
+          date={items[3].date}
+          amount={items[3].amount}
+        />
+      </Card>
+    </div>
   );
 }
---
+++ src/components/expenses/ExpensesFilter.jsx
@@ -0,0 +1,26 @@
+import '../../styles/ExpensesFilter.css'
+
+
+const ExpensesFilter = (props) => {
+  const dropdownChangeHandler = (event) => {
+    props.onChangeFilter(event.target.value);
+  };
+
+  return (
+    <div className="expenses-filter">
+      <div className="expenses-filter__control">
+        <label>Filter by year</label>
+        <select value={props.selected} onChange={dropdownChangeHandler}>
+            {
+                props.allYears.map((year, index) => {
+                    return <option key={index} value={year}>{year}</option>;
+                })
+            }
+          
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default ExpensesFilter;
